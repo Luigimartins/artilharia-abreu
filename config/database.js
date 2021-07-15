6c7eb0daf415736de1c7e5f4939f1e945c0f3442
Luigimartins <luigi.martins.355@gmail.com>
2021-07-15 00:11:20 -0300

    teste heroku

--- config/database.js
+++ config/database.js
@@ -71,11 +71,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: 'ec2-3-231-69-204.compute-1.amazonaws.com',
-      port: '5432',
-      user: 'pwrsjvgiqpefyd',
-      password: 'd2856dffe2116a5b39159bd13eed2bbd7bd842ee61aaae88a9d5f79e949f8257',
-      database: 'dcshmd5fpjr9ac',
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis'),
       ssl: { rejectUnauthorized: false }
     }
   }
